00cd4dfd9a537c36f78618022a9a6bd97c9693b4
Синельник Тимур Романович <71385842+Falokut@users.noreply.github.com>
2024-09-15 13:34:30 +0300

    Add telegram_bot limit configuration

--- config/telegram_bot.go
+++ config/telegram_bot.go
@@ -5,4 +5,5 @@ type TelegramBotConfig struct {
 	PaymentToken string `yaml:"payment_token" env:"TG_BOT_PAYMENT_TOKEN"`
 	// timeout in seconds
 	Timeout int `yaml:"timeout" env:"TG_BOT_TIMEOUT"`
+	Limit   int `yaml:"limit" env:"TG_BOT_LIMIT"`
 }
