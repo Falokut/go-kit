40c9e17227be88b01eb4b4d78cea2a1967445306
Синельник Тимур Романович <71385842+Falokut@users.noreply.github.com>
2025-09-19 18:06:55 +0300

    Update file_data.go

--- http/types/file_data.go
+++ http/types/file_data.go
@@ -20,13 +20,18 @@ type FileData struct {
 	ContentReader   io.ReadSeekCloser
 }
 
-func (file FileData) Write(w http.ResponseWriter) error {
+// Write отправляет содержимое файла в ResponseWriter, поддерживая частичные загрузки.
+func (file *FileData) Write(w http.ResponseWriter) error {
+	if file.ContentReader == nil {
+		return errors.New("ContentReader is nil")
+	}
 	defer file.ContentReader.Close()
+
 	if file.PartialDataInfo != nil {
 		return file.writePartialData(w)
 	}
 
-	w.Header().Set("Content-Type", file.ContentType)
+	file.setHeaders(w)
 	w.WriteHeader(http.StatusOK)
 
 	_, err := io.Copy(w, file.ContentReader)
@@ -36,32 +41,40 @@ func (file FileData) Write(w http.ResponseWriter) error {
 	return nil
 }
 
-func (file FileData) writePartialData(w http.ResponseWriter) error {
-	partialInfo := file.PartialDataInfo
-	startByte := partialInfo.RangeStartByte
-	endByte := partialInfo.RangeEndByte
+// writePartialData отправляет только часть файла, согласно PartialDataInfo.
+func (file *FileData) writePartialData(w http.ResponseWriter) error {
+	start := file.PartialDataInfo.RangeStartByte
+	end := file.PartialDataInfo.RangeEndByte
 
-	if endByte == 0 || endByte >= file.TotalFileSize {
-		endByte = file.TotalFileSize - 1
+	if start >= file.TotalFileSize {
+		return errors.New("start byte out of range")
+	}
+	if end == 0 || end >= file.TotalFileSize {
+		end = file.TotalFileSize - 1
 	}
 
-	contentLength := endByte - startByte + 1
+	contentLength := end - start + 1
 
-	w.Header().Set("Content-Type", file.ContentType)
+	file.setHeaders(w)
 	w.Header().Set("Accept-Ranges", "bytes")
-	w.Header().Set("Content-Range", fmt.Sprintf("bytes %d-%d/%d", startByte, endByte, file.TotalFileSize))
+	w.Header().Set("Content-Range", fmt.Sprintf("bytes %d-%d/%d", start, end, file.TotalFileSize))
 	w.Header().Set("Content-Length", fmt.Sprintf("%d", contentLength))
 	w.WriteHeader(http.StatusPartialContent)
 
-	_, err := file.ContentReader.Seek(startByte, io.SeekStart)
+	_, err := file.ContentReader.Seek(start, io.SeekStart)
 	if err != nil {
 		return errors.WithMessage(err, "failed to seek start byte")
 	}
 
 	_, err = io.CopyN(w, file.ContentReader, contentLength)
-	if err != nil {
+	if err != nil && !errors.Is(err, io.EOF) {
 		return errors.WithMessage(err, "error during content copying")
 	}
 
 	return nil
+}
+
+// setHeaders устанавливает основные HTTP-заголовки для файла.
+func (file *FileData) setHeaders(w http.ResponseWriter) {
+	w.Header().Set("Content-Type", file.ContentType)
 }
