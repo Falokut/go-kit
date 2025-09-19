ea618cf8a124983bb31e6349e1410c41844e1fb2
Синельник Тимур Романович <71385842+Falokut@users.noreply.github.com>
2025-09-19 18:06:55 +0300

    Update file_data.go

--- http/types/file_data.go
+++ http/types/file_data.go
@@ -17,48 +17,50 @@ type FileData struct {
 	PartialDataInfo *PartialDataInfo
 	ContentType     string
 	TotalFileSize   int64
-	ContentReader   io.ReadSeekCloser
+	ContentReader   io.ReadCloser
 }
 
+// Write отправляет файл в HTTP-ответ.
+// Поддерживает полный файл или частичный Range.
 func (file FileData) Write(w http.ResponseWriter) error {
 	defer file.ContentReader.Close()
+
 	if file.PartialDataInfo != nil {
 		return file.writePartialData(w)
 	}
 
+	// Полный файл
 	w.Header().Set("Content-Type", file.ContentType)
+	w.Header().Set("Content-Length", fmt.Sprintf("%d", file.TotalFileSize))
 	w.WriteHeader(http.StatusOK)
 
 	_, err := io.Copy(w, file.ContentReader)
 	if err != nil {
 		return errors.WithMessage(err, "error during content copying")
 	}
+
 	return nil
 }
 
+// writePartialData отправляет только указанный диапазон байт
 func (file FileData) writePartialData(w http.ResponseWriter) error {
-	partialInfo := file.PartialDataInfo
-	startByte := partialInfo.RangeStartByte
-	endByte := partialInfo.RangeEndByte
+	partial := file.PartialDataInfo
+	start := partial.RangeStartByte
+	end := partial.RangeEndByte
 
-	if endByte == 0 || endByte >= file.TotalFileSize {
-		endByte = file.TotalFileSize - 1
+	if end == 0 || end >= file.TotalFileSize {
+		end = file.TotalFileSize - 1
 	}
 
-	contentLength := endByte - startByte + 1
+	contentLength := end - start + 1
 
 	w.Header().Set("Content-Type", file.ContentType)
 	w.Header().Set("Accept-Ranges", "bytes")
-	w.Header().Set("Content-Range", fmt.Sprintf("bytes %d-%d/%d", startByte, endByte, file.TotalFileSize))
+	w.Header().Set("Content-Range", fmt.Sprintf("bytes %d-%d/%d", start, end, file.TotalFileSize))
 	w.Header().Set("Content-Length", fmt.Sprintf("%d", contentLength))
 	w.WriteHeader(http.StatusPartialContent)
 
-	_, err := file.ContentReader.Seek(startByte, io.SeekStart)
-	if err != nil {
-		return errors.WithMessage(err, "failed to seek start byte")
-	}
-
-	_, err = io.CopyN(w, file.ContentReader, contentLength)
+	_, err := io.Copy(w, file.ContentReader)
 	if err != nil {
 		return errors.WithMessage(err, "error during content copying")
 	}
